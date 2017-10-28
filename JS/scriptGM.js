809496196f8f88d48eb11489da998e76f0a25cc4
Neil Cudden <ncudden@sbcglobal.net>
2017-10-27 17:34:40 -0700

    Initial commit of google maps

--- JS/scriptGM.js
+++ JS/scriptGM.js
@@ -1,15 +1,25 @@
 var latitude = 33.326714;
 var longitude = -111.609777;
 
-function myMap() {
+/*function myMap() {
     var mapProp= {center:new google.maps.LatLng(latitude,longitude),zoom:10,};
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-} 
+} */
   
 /*$(function(){*/
-    $("form").on("submit",function(event){
+    $(".button").on("click",function(event){
         latitude = $('#latitude').val();
         longitude = $('#longitude').val();
         console.log('Latitude: '+latitude+', Longitude: '+longitude);
-    });   
+        function myMap() {
+            var mapProp= {center:new google.maps.LatLng(latitude,longitude),zoom:10,};
+            var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            }
+    }); 
+
+
+function myMap() {
+    var mapProp= {center:new google.maps.LatLng(latitude,longitude),zoom:10,};
+    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
 /*});*/
