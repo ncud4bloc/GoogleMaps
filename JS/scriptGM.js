721b22466057c53257e1d2fea5973bd04619a34b
Neil Cudden <ncudden@sbcglobal.net>
2017-10-30 12:52:35 -0700

    Baseline 10-30-17 commit

--- JS/scriptGM.js
+++ JS/scriptGM.js
@@ -1,25 +1,16 @@
-var latitude = 33.326714;
-var longitude = -111.609777;
+var latitude;
+var longitude;
 
-/*function myMap() {
-    var mapProp= {center:new google.maps.LatLng(latitude,longitude),zoom:10,};
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-} */
+$(function(){
+
+    function myMap() {
+        var mapProp= {center:new google.maps.LatLng(latitude,longitude),zoom:10,};
+        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    } 
   
-/*$(function(){*/
     $(".button").on("click",function(event){
         latitude = $('#latitude').val();
         longitude = $('#longitude').val();
         console.log('Latitude: '+latitude+', Longitude: '+longitude);
-        function myMap() {
-            var mapProp= {center:new google.maps.LatLng(latitude,longitude),zoom:10,};
-            var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-            }
     }); 
-
-
-function myMap() {
-    var mapProp= {center:new google.maps.LatLng(latitude,longitude),zoom:10,};
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-/*});*/
+});
